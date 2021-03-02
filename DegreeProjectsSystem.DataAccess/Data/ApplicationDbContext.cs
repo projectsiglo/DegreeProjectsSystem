5fefad8b5e7c86f922f3b74d85c5a7a5848bc7c4
projectsiglo <project.siglo@gmail.com>
2021-03-01 21:44:42 -0500

    Separación de la capa de datos

--- DegreeProjectsSystem.DataAccess/Data/ApplicationDbContext.cs
+++ DegreeProjectsSystem.DataAccess/Data/ApplicationDbContext.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace DegreeProjectsSystem.Data
+namespace DegreeProjectsSystem.DegreeProjectsSystem.DataAccess.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
