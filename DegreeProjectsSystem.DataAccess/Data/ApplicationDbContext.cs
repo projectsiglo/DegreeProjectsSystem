925f6299b180c3f80635edb9f994bcaf8c8ead0d
projectsiglo <project.siglo@gmail.com>
2021-03-03 17:58:13 -0500

    Agregada migración AddDepartmentsToDatabase y modelo Department  para la creación de su respectiva tabla en la base de datos.

--- DegreeProjectsSystem.DataAccess/Data/ApplicationDbContext.cs
+++ DegreeProjectsSystem.DataAccess/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using DegreeProjectsSystem.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace DegreeProjectsSystem.DegreeProjectsSystem.DataAccess.Data
@@ -9,5 +10,7 @@ namespace DegreeProjectsSystem.DegreeProjectsSystem.DataAccess.Data
             : base(options)
         {
         }
+
+        public DbSet<Department> Departments { get; set; }
     }
 }
---
+++ DegreeProjectsSystem.Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DegreeProjectsSystem.Models
+{
+    public class Department
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener menos de {1} caracteres")]
+        [Display(Name ="Nombre")]
+        public string Name { get; set; }
+    }
+}
