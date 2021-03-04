8884010588bb2989040585a38b4e16b4b8092d0a
projectsiglo <project.siglo@gmail.com>
2021-03-03 22:08:39 -0500

    Departamentos Index y la acción Obtener Todos

--- DegreeProjectsSystem/Areas/Admin/Controllers/DepartmentController.cs
+++ DegreeProjectsSystem/Areas/Admin/Controllers/DepartmentController.cs
@@ -2,15 +2,30 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DegreeProjectsSystem.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DegreeProjectsSystem.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class DepartmentController : Controller
     {
+        private readonly IUnitWork _unitWork;
+
+        public DepartmentController(IUnitWork unitWork)
+        {
+            _unitWork = unitWork;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        #region API
+        public IActionResult GetAllDepartments() 
+        {
+            var departments = _unitWork.Department.GetAll();
+        }
+        #endregion
     }
 }
