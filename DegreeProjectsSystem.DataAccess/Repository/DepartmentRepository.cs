59b3fa247b8d8af7d846a4aa695b3bbb05f043b4
projectsiglo <project.siglo@gmail.com>
2021-03-03 20:51:40 -0500

    Interfaz y repositorio bodega

---
+++ DegreeProjectsSystem.DataAccess/Repository/DepartmentRepository.cs
@@ -0,0 +1,31 @@
+﻿using DegreeProjectsSystem.DataAccess.Repository.IRepository;
+using DegreeProjectsSystem.DegreeProjectsSystem.DataAccess.Data;
+using DegreeProjectsSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DegreeProjectsSystem.DataAccess.Repository
+{
+    public class DepartmentRepository : Repository<Department>, IDepartmentRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public DepartmentRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Department department)
+        {
+            var departmentDb = _db.Departments.FirstOrDefault(d => d.Id == department.Id);
+            if (departmentDb !=null)
+            {
+                departmentDb.Name = department.Name;
+
+                _db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DegreeProjectsSystem.DataAccess/Repository/IRepository/IDepartmentRepository.cs
@@ -0,0 +1,12 @@
+﻿using DegreeProjectsSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DegreeProjectsSystem.DataAccess.Repository.IRepository
+{
+    public interface IDepartmentRepository : IRepository<Department>
+    {
+        void Update(Department department);
+    }
+}
